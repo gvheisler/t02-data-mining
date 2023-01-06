ff4c4746e175c0664a5f800ea86ca47ef2943d1c
gvheisler <gvheisler@inf.ufsm.br>
2023-01-06 17:55:56 -0300

    plot de casos e obitos (ta ruim de enxergar)

--- script.r
+++ script.r
@@ -3,12 +3,14 @@ ds <- read.csv('C:\\Users\\gvhei\\Desktop\\projetos\\saudeRS_2022.csv', sep = ';
 
 obitos <- ds[which(ds$EVOLUCAO=='OBITO'),]
 
-nds <- ds[,-c(1:4, 7:9, 11, 21:30)]
+nds <- ds[,-c(1:4, 7, 9:11, 21:30)]
 obitos <- obitos[,-c(1:4, 7:10, 21:30)]
 
 obitos$DATA_EVOLUCAO <- as.Date(obitos$DATA_EVOLUCAO, format = "%d/%m/%y")
+nds$DATA_CONFIRMACAO <- as.Date(nds$DATA_CONFIRMACAO, format = "%d/%m/%y")
 
 obitos <- obitos[order(obitos$DATA_EVOLUCAO),]
+nds <- nds[order(nds$DATA_CONFIRMACAO),]
 
 obitosPorDia <- data.frame(matrix(nrow = length(unique(obitos$DATA_EVOLUCAO)), ncol = 2))
 
@@ -16,10 +18,23 @@ colnames(obitosPorDia) <- c('dia', 'obitos')
 
 obitosPorDia$dia <- unique(obitos$DATA_EVOLUCAO)
 
-
 for(i in 1:nrow(obitosPorDia)){
   dsAux <- obitos[which(obitos$DATA_EVOLUCAO==obitosPorDia[i,1]), ]
   obitosPorDia[i,2] <- nrow(dsAux)
 }
 
-plot(obitosPorDia, type = 'l')
+casosPorDia <- data.frame(matrix(nrow = length(unique(nds$DATA_CONFIRMACAO)), ncol = 2))
+
+colnames(casosPorDia) <- c('dia', 'casos')
+
+casosPorDia$dia <- unique(nds$DATA_CONFIRMACAO)
+
+for(i in 1:nrow(obitosPorDia)){
+  dsAux <- nds[which(nds$DATA_CONFIRMACAO==obitosPorDia[i,1]), ]
+  casosPorDia[i,2] <- nrow(dsAux)
+}
+
+plot(casosPorDia, type = 'l')
+casosPorDia[250,2] <- 0
+obitosPorDia[300,2] <- 2500
+lines(obitosPorDia, col = 'red')
