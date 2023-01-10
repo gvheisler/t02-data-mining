45e9e38bbf2ceb03b98c3087b991db07bd8cba8f
gvheisler <gvheisler@inf.ufsm.br>
2023-01-10 09:10:47 -0300

    imagens dos plots adicionadas

--- geracao_graficos/casosVideo.r
+++ geracao_graficos/casosVideo.r
@@ -1,9 +1,10 @@
 library(ggplot2)
 
-setwd('C:\\Users\\gvhei\\Desktop\\plots')
+#setwd('C:\\Users\\gvhei\\Desktop\\plots')
+setwd('C:\\Users\\gvheisler\\Desktop\\data-mining\\t02-data-mining\\graficos\\videos\\plots\\evolucaoCasos')
 
-#ds <- read.csv2('C:\\Users\\gvheisler\\Desktop\\data-mining\\t02-data-mining\\ds\\casosObitosDia.csv')
-ds <- read.csv2('C:\\Users\\gvhei\\Desktop\\projetos\\t02-data-mining\\ds\\casosObitosDia.csv')
+ds <- read.csv2('C:\\Users\\gvheisler\\Desktop\\data-mining\\t02-data-mining\\ds\\casosObitosDia.csv')
+#ds <- read.csv2('C:\\Users\\gvhei\\Desktop\\projetos\\t02-data-mining\\ds\\casosObitosDia.csv')
 
 ds$data <- as.Date(ds$data, format = "%Y-%m-%d")
 #ds <- ds[which(ds$data>=as.Date("2023-01-01", format = '%Y-%m-%d')),]
@@ -16,14 +17,3 @@ for(dia in ds$data){
        ylab = "Quantidade de casos", xlab = "Data", main = "Evolução da COVID-19 no RS")
   dev.off()
 }
-
-plot(x = ds$data, y = ds$quant_casos, type = 'l', xlim = c(min(ds$data), max(ds$data)), ylim = c(min(ds$quant_casos), max(ds$quant_casos)))
-
-qplot(
-  x = ds$data,
-  y = ds$quant_casos,
-  geom = c("line"),
-  ylab = "Quantidade de Casos",
-  xlab = "Data",
-  main = ""
-)
---
+++ geracao_graficos/obitosVideo.R
@@ -0,0 +1,30 @@
+library(ggplot2)
+
+#setwd('C:\\Users\\gvhei\\Desktop\\plots')
+setwd('C:\\Users\\gvheisler\\Desktop\\plots')
+
+ds <- read.csv2('C:\\Users\\gvheisler\\Desktop\\data-mining\\t02-data-mining\\ds\\casosObitosDia.csv')
+#ds <- read.csv2('C:\\Users\\gvhei\\Desktop\\projetos\\t02-data-mining\\ds\\casosObitosDia.csv')
+
+ds$data <- as.Date(ds$data, format = "%Y-%m-%d")
+#ds <- ds[which(ds$data>=as.Date("2023-01-01", format = '%Y-%m-%d')),]
+
+for(dia in ds$data){
+  dsAux <- ds[which(ds$data<=dia),]
+  png(paste("obitos-", dia, ".png", sep = ''))
+  plot(x = dsAux$data, y = dsAux$quant_obitos, type = 'l', xlim = c(min(ds$data), max(ds$data)),
+       ylim = c(min(ds$quant_obitos), max(ds$quant_obitos)), lwd = 1, 
+       ylab = "Quantidade de óbitos", xlab = "Data", main = "Evolução da COVID-19 no RS")
+  dev.off()
+}
+
+plot(x = ds$data, y = ds$quant_casos, type = 'l', xlim = c(min(ds$data), max(ds$data)), ylim = c(min(ds$quant_casos), max(ds$quant_casos)))
+
+qplot(
+  x = ds$data,
+  y = ds$quant_casos,
+  geom = c("line"),
+  ylab = "Quantidade de Casos",
+  xlab = "Data",
+  main = ""
+)
