ad24587d573c6636234de99e2d48cd753f52d6ad
gvheisler <gvheisler@inf.ufsm.br>
2023-01-23 09:41:42 -0300

    grafico de porcentagem de sintomas adicionado

--- codigos/geracao_graficos/TS_sintomas.R
+++ codigos/geracao_graficos/TS_sintomas.R
@@ -1,4 +1,7 @@
-completo <- read.csv2('C:\\Users\\gvhei\\Desktop\\projetos\\completo.csv')
+#completo <- read.csv2('C:\\Users\\gvhei\\Desktop\\projetos\\completo.csv')
+completo <- read.csv2('C:\\Users\\gvheisler\\Desktop\\data-mining\\completo.csv')
+
+
 
 ds <- completo
 
@@ -8,27 +11,33 @@ ds$DATA_CONFIRMACAO <- as.Date(ds$DATA_CONFIRMACAO, format = "%d/%m/%Y")
 
 dias <- seq(as.Date(min(ds$DATA_CONFIRMACAO)), as.Date(max(ds$DATA_CONFIRMACAO)), by = 'days')
 
-sintomasTS <- data.frame(matrix(nrow = length(dias), ncol = 8))
+sintomasTS <- data.frame(matrix(nrow = length(dias), ncol = 7))
 
-colnames(sintomasTS) <- c('dia','casos','obitos','febre','tosse','garganta','dispneia','outros')
+colnames(sintomasTS) <- c('dia','casos','febre','tosse','garganta','dispneia','outros')
 
 sintomasTS$dia <- dias
 
 for (i in 1:nrow(sintomasTS)) {
   dfAux <- ds[which(ds$DATA_CONFIRMACAO==sintomasTS[i,1]),]
   sintomasTS[i,2] <- nrow(dfAux)
-  sintomasTS[i,3] <- nrow(dfAux[which(dfAux$EVOLUCAO=='OBITO'),])
-  sintomasTS[i,4] <- nrow(dfAux[which(dfAux$FEBRE=='SIM'),])
-  sintomasTS[i,5] <- nrow(dfAux[which(dfAux$TOSSE=='SIM'),])
-  sintomasTS[i,6] <- nrow(dfAux[which(dfAux$GARGANTA=='SIM'),])
-  sintomasTS[i,7] <- nrow(dfAux[which(dfAux$DISPNEIA=='SIM'),])
-  sintomasTS[i,8] <- nrow(dfAux[which(dfAux$OUTROS=='SIM'),])
+  sintomasTS[i,3] <- nrow(dfAux[which(dfAux$FEBRE=='SIM'),])
+  sintomasTS[i,4] <- nrow(dfAux[which(dfAux$TOSSE=='SIM'),])
+  sintomasTS[i,5] <- nrow(dfAux[which(dfAux$GARGANTA=='SIM'),])
+  sintomasTS[i,6] <- nrow(dfAux[which(dfAux$DISPNEIA=='SIM'),])
+  sintomasTS[i,7] <- nrow(dfAux[which(dfAux$OUTROS=='SIM'),])
 }
 
-sintomasCasos <- sintomasTS[,-3]
 
+sintomasTS <- sintomasTS[which(sintomasTS$casos>0),]
 
-for (j in 3:ncol(sintomasTS)-1) {
-  sintomasCasos[,j] <- (sintomasCasos[,j]*100)/sintomasCasos[,2]
+for (j in 3:ncol(sintomasTS)) {
+  sintomasTS[,j] <- ((sintomasTS[,j]*100)/sintomasTS[,2])
 }
 
+plot(x = sintomasTS$dia, sintomasTS$febre, type = 'l', col = 'blue', ylim = c(0,100), xlab = 'Data', ylab = 'Porcentagem de casos', lwd = 2)
+lines(x = sintomasTS$dia, sintomasTS$tosse, col = 'darkgreen', lwd = 2)
+lines(x = sintomasTS$dia, sintomasTS$garganta, col = 'black', lwd = 2)
+lines(x = sintomasTS$dia, sintomasTS$dispneia, col = 'darkred', lwd = 2)
+lines(x = sintomasTS$dia, sintomasTS$outros, col = 'yellow', lwd = 2)
+legend('topleft', legend=c("Febre", "Tosse", "Garganta", "Dispneia", "Outros"),
+       col=c("blue", "darkgreen", "black", 'darkred', 'yellow'), lty = 1, lwd = 2, cex = 0.5)
